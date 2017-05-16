5fc6dc33253cae01277f89fe9d8e2b12133e99a5
Astor-Bizard <astor.bizard@etu.univ-grenoble-alpes.fr>
2017-05-16 15:28:55 +0200

    Recuperation des donnees de la camera

--- catkin_ws/src/securisator/src/securisator_node.cpp
+++ catkin_ws/src/securisator/src/securisator_node.cpp
@@ -1,14 +1,21 @@
 #include "ros/ros.h"
 #include "sensor_msgs/Image.h"
+#include "sensor_msgs/CameraInfo.h"
+#include "geometry_msgs/Point.h"
 
 #define PERSON_IMG_TOPIC "/clusterisator/person"
+#define CAMERA_INFO_TOPIC "/depth/camera_info"
 
 class Securisator {
 
 private:
 ros::Subscriber person_img_sub;
+ros::Subscriber camera_info_sub;
 
 uint32_t h,w;
+
+double f;
+double cx,cy;
 
 public:
 
@@ -17,7 +24,21 @@ Securisator(){
 	ros::NodeHandle nh;
 
 	person_img_sub = nh.subscribe(PERSON_IMG_TOPIC, 1, &Securisator::personImageCallback, this);
+	camera_info_sub = nh.subscribe(CAMERA_INFO_TOPIC, 1, &Securisator::cameraInfoCallback, this);
 
+}
+
+void cameraInfoCallback(const sensor_msgs::CameraInfo::ConstPtr& camera_info){
+	f = camera_info->K[0];
+	cx = camera_info->K[2];
+	cx = camera_info->K[5];
+}
+
+geometry_msgs::Point depth_to_cartesian(int x, int y, uint16_t depth){
+	geometry_msgs::Point p;
+	p.x = ((x-cx)*depth)/f;
+	p.y = ((y-cy)*depth)/f;
+	p.z = depth;
 }
 
 void personImageCallback(const sensor_msgs::Image::ConstPtr& img){
@@ -33,7 +54,7 @@ void personImageCallback(const sensor_msgs::Image::ConstPtr& img){
 				found = true;
 	
 	if(found)
-		ROS_INFO("Found !");	
+		ROS_INFO("Found !");
 }
 
 };
