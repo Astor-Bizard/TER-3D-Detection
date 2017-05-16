c12b8b70083616aad63eaa3b61b6ceeb90f682b2
Astor-Bizard <astor.bizard@etu.univ-grenoble-alpes.fr>
2017-05-16 16:40:30 +0200

    Creation du msg et projection cartesienne du robot

--- catkin_ws/src/securisator/src/securisator_node.cpp
+++ catkin_ws/src/securisator/src/securisator_node.cpp
@@ -3,8 +3,17 @@
 #include "sensor_msgs/CameraInfo.h"
 #include "geometry_msgs/Point.h"
 
+#include <math.h>
+
 #define PERSON_IMG_TOPIC "/clusterisator/person"
 #define CAMERA_INFO_TOPIC "/depth/camera_info"
+
+#define MAX_ROBOT_SIZE 8000
+
+#define STATIC_OBJECT 0
+#define MOVING_OBJECT 1
+#define MOVING_PERSON 2
+#define ROBOT         3
 
 class Securisator {
 
@@ -16,6 +25,8 @@ uint32_t h,w;
 
 double f;
 double cx,cy;
+
+geometry_msgs::Point robot[MAX_ROBOT_SIZE];
 
 public:
 
@@ -39,6 +50,7 @@ geometry_msgs::Point depth_to_cartesian(int x, int y, uint16_t depth){
 	p.x = ((x-cx)*depth)/f;
 	p.y = ((y-cy)*depth)/f;
 	p.z = depth;
+	return p;
 }
 
 void personImageCallback(const sensor_msgs::Image::ConstPtr& img){
@@ -47,14 +59,45 @@ void personImageCallback(const sensor_msgs::Image::ConstPtr& img){
 	
 	uint32_t i,j;
 	
-	bool found = false;
-	for(i=0;i<h;i++)
-		for(j=0;j<w;j++)
-			if(img->data[i*3*w+j*3+2] > 0)
-				found = true;
-	
-	if(found)
-		ROS_INFO("Found !");
+	bool found_robot = false;
+	int i_robot = 0;
+	geometry_msgs::Point min_robot,max_robot;
+	for(i=0;i<h;i++){
+		for(j=0;j<w;j++){
+			uint8_t type = img->data[i*3*w+j*3+2];
+			uint16_t depth = (img->data[i*3*w + j*3 + 1] << 8) + img->data[i*3*w + j*3];
+			if(type == ROBOT){
+				robot[i_robot] = depth_to_cartesian(i,j,depth);
+				if(!found_robot){
+					min_robot.x = robot[i_robot].x;
+					min_robot.y = robot[i_robot].y;
+					min_robot.z = robot[i_robot].z;
+					max_robot.x = robot[i_robot].x;
+					max_robot.y = robot[i_robot].y;
+					max_robot.z = robot[i_robot].z;
+					found_robot = true;
+				}
+				else{
+					min_robot.x = fmin(robot[i_robot].x,min_robot.x);
+					min_robot.y = fmin(robot[i_robot].y,min_robot.y);
+					min_robot.z = fmin(robot[i_robot].z,min_robot.z);
+					max_robot.x = fmax(robot[i_robot].x,max_robot.x);
+					max_robot.y = fmax(robot[i_robot].y,max_robot.y);
+					max_robot.z = fmax(robot[i_robot].z,max_robot.z);
+				}
+				i_robot++;
+			}
+		}
+	}
+	//     ^
+	//   x |
+	//     |  y
+	//     @---->
+	//    /
+	//   / z
+	//  v
+	if(found_robot)
+		ROS_INFO("Found robot of size %d between (%d,%d,%d) and (%d,%d,%d)",i_robot,(int)min_robot.x,(int)min_robot.y,(int)min_robot.z,(int)max_robot.x,(int)max_robot.y,(int)max_robot.z);
 }
 
 };
