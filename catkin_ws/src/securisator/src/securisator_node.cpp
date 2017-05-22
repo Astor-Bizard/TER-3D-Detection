ac13f9c00c937f1fe223cf61341ed9d2cbfdc58b
Astor-Bizard <astor.bizard@etu.univ-grenoble-alpes.fr>
2017-05-22 16:01:07 +0200

    Nettoyage de securisator

--- catkin_ws/src/securisator/src/securisator_node.cpp
+++ catkin_ws/src/securisator/src/securisator_node.cpp
@@ -28,16 +28,21 @@ int resolution_factor;
 double f;
 double cx,cy;
 
-struct robot_t{
-	geometry_msgs::Point pt_min_x;
-	geometry_msgs::Point pt_min_y;
-	geometry_msgs::Point pt_min_z;
-	geometry_msgs::Point pt_max_x;
-	geometry_msgs::Point pt_max_y;
-	geometry_msgs::Point pt_max_z;
-};
+#define I_MIN_X 0
+#define I_MIN_Y 1
+#define I_MIN_Z 2
+#define I_MAX_X 3
+#define I_MAX_Y 4
+#define I_MAX_Z 5
+geometry_msgs::Point robot[6];
 
-struct robot_t robot;
+uint8_t CODE_STATIC_OBJECT;
+uint8_t CODE_MOVING_OBJECT;
+uint8_t CODE_MOVING_PERSON;
+uint8_t CODE_ROBOT;
+
+std::vector<uint8_t> data_type;
+std::vector<uint16_t> data_depth;
 
 public:
 
@@ -66,53 +71,35 @@ geometry_msgs::Point depth_to_cartesian(int x, int y, uint16_t depth){
 	return p;
 }
 
-void collect_robot_data(const clusterisator::Persons::ConstPtr& img_persons){
-
-	const sensor_msgs::Image img = img_persons->img;
-	
-	recieved_data_pub.publish(img);
+void collect_robot_data(){
 
 	uint32_t i,j;
 	
-	resolution_factor = img_persons->resolution_factor;
-
+	geometry_msgs::Point cartesian_map[h*w];	
+	uint32_t index_in_cartesian_map[6];
+	
 	bool found_robot = false;
-	
-	uint8_t data_type[h*w];
-	uint16_t data_depth[h*w];
-	
-	for(i=0;i<h;i++){
-		for(j=0;j<w;j++){
-			data_type[i*w+j] = img.data[i*3*w+j*3+2];
-			data_depth[i*w+j] = (img.data[i*3*w + j*3 + 1] << 8) + img.data[i*3*w + j*3];
-		}
-	}
-	
-	geometry_msgs::Point cartesian_map[h*w];
-	uint32_t index_min_x, index_min_y, index_min_z, index_max_x, index_max_y, index_max_z;
-	
 	for(i=0;i<h;i++){
 		for(j=0;j<w;j++){
 			uint8_t type = data_type[i*w+j];
 			uint16_t depth = data_depth[i*w+j];
-			if(type == img_persons->ROBOT){
+			if(type == CODE_ROBOT){
 				geometry_msgs::Point current = depth_to_cartesian(i,j,depth);
 				cartesian_map[i*w+j] = current;
 				if(!found_robot){
-					index_min_x = index_max_x =
-					index_min_y = index_max_y =
-					index_min_z = index_max_z =
-												i*w+j;
+					for(short int k=0;k<6;k++)
+						index_in_cartesian_map[k] = i*w+j;
+					
 					found_robot = true;
 				}
 				else{
-					#define MAJ_INDEX(INDEX,XYZ,CMP) INDEX = cartesian_map[INDEX].XYZ CMP current.XYZ ? INDEX : i*w+j;
-					MAJ_INDEX(index_min_x,x,<);
-					MAJ_INDEX(index_min_y,y,<);
-					MAJ_INDEX(index_min_z,z,<);
-					MAJ_INDEX(index_max_x,x,>);
-					MAJ_INDEX(index_max_y,y,>);
-					MAJ_INDEX(index_max_z,z,>);
+					#define MAJ_INDEX(INDEX,XYZ,CMP) index_in_cartesian_map[INDEX] = cartesian_map[index_in_cartesian_map[INDEX]].XYZ CMP current.XYZ ? index_in_cartesian_map[INDEX] : i*w+j;
+					MAJ_INDEX(I_MIN_X,x,<);
+					MAJ_INDEX(I_MIN_Y,y,<);
+					MAJ_INDEX(I_MIN_Z,z,<);
+					MAJ_INDEX(I_MAX_X,x,>);
+					MAJ_INDEX(I_MAX_Y,y,>);
+					MAJ_INDEX(I_MAX_Z,z,>);
 				}
 			}
 		}
@@ -125,16 +112,12 @@ void collect_robot_data(const clusterisator::Persons::ConstPtr& img_persons){
 	//   / z
 	//  v
 	
-	robot.pt_min_x = cartesian_map[index_min_x];
-	robot.pt_min_y = cartesian_map[index_min_y];
-	robot.pt_min_z = cartesian_map[index_min_z];
-	robot.pt_max_x = cartesian_map[index_max_x];
-	robot.pt_max_y = cartesian_map[index_max_y];
-	robot.pt_max_z = cartesian_map[index_max_z];
+	for(short int k=0;k<6;k++)
+		robot[k] = cartesian_map[index_in_cartesian_map[k]];
 	
 	if(found_robot){
-		ROS_INFO("Found robot at position (%f,%f,%f)", robot.pt_min_x.x + (robot.pt_max_x.x-robot.pt_min_x.x)/2.0f, robot.pt_min_y.y + (robot.pt_max_y.y-robot.pt_min_y.y)/2.0f, robot.pt_min_z.z + (robot.pt_max_z.z-robot.pt_min_z.z)/2.0f);
-		ROS_INFO("Robot size : (%f,%f,%f)",(robot.pt_max_x.x-robot.pt_min_x.x),(robot.pt_max_y.y-robot.pt_min_y.y),(robot.pt_max_z.z-robot.pt_min_z.z));
+		ROS_INFO("Found robot at position (%f,%f,%f)", robot[I_MIN_X].x + (robot[I_MAX_X].x-robot[I_MIN_X].x)/2.0f, robot[I_MIN_Y].y + (robot[I_MAX_Y].y-robot[I_MIN_Y].y)/2.0f, robot[I_MIN_Z].z + (robot[I_MAX_Z].z-robot[I_MIN_Z].z)/2.0f);
+		ROS_INFO("Robot size : (%f,%f,%f)",(robot[I_MAX_X].x-robot[I_MIN_X].x),(robot[I_MAX_Y].y-robot[I_MIN_Y].y),(robot[I_MAX_Z].z-robot[I_MIN_Z].z));
 	}
 }
 
@@ -142,38 +125,60 @@ static uint16_t distance(const geometry_msgs::Point a, const geometry_msgs::Poin
 	return sqrt(pow((a.x - b.x),2) + pow((a.y - b.y),2) + pow((a.z - b.z),2));
 }
 
-void personImageCallback(const clusterisator::Persons::ConstPtr& img_persons){
-	sensor_msgs::Image img = img_persons->img;
-	h = img.height;
-	w = img.width;
+void check_security_distance(){
 	
-	uint32_t i,j,k;
+	uint32_t i,j;
 	
-	if(img_persons->there_is_a_robot){
-		collect_robot_data(img_persons);
-
-		bool danger = false;
-		for(i=0;i<h && !danger;i++){
-			for(j=0;j<w && !danger;j++){
-				uint8_t type = img.data[i*3*w+j*3+2];
-				uint16_t depth = (img.data[i*3*w + j*3 + 1] << 8) + img.data[i*3*w + j*3];
-				if(type == img_persons->MOVING_PERSON){
-					geometry_msgs::Point p = depth_to_cartesian(i,j,depth);
-					uint16_t dist;
-					#define CHECK_DISTANCE_WITH_P(PT)	dist = distance(p,PT);\
-														if(dist < SECURITY_DISTANCE){\
-															ROS_WARN("BAAAAH ATTENTION ! Distance : %d",dist);\
-															danger = true;\
-														}
-					CHECK_DISTANCE_WITH_P(robot.pt_min_x);
-					CHECK_DISTANCE_WITH_P(robot.pt_min_y);
-					CHECK_DISTANCE_WITH_P(robot.pt_min_z);
-					CHECK_DISTANCE_WITH_P(robot.pt_max_x);
-					CHECK_DISTANCE_WITH_P(robot.pt_max_y);
-					CHECK_DISTANCE_WITH_P(robot.pt_max_z);
+	bool danger = false;
+	for(i=0;i<h && !danger;i++){
+		for(j=0;j<w && !danger;j++){
+			uint8_t type = data_type[i*w+j];
+			uint16_t depth = data_depth[i*w+j];
+			if(type == CODE_MOVING_PERSON){
+				geometry_msgs::Point p = depth_to_cartesian(i,j,depth);
+				uint16_t dist;
+				for(short int k=0; k<6 && !danger; k++){
+					dist = distance(p,robot[k]);
+					if(dist < SECURITY_DISTANCE){
+						ROS_WARN("BAAAAH ATTENTION ! Distance : %d",dist);
+						danger = true;
+					}
 				}
 			}
 		}
+	}
+}
+
+void personImageCallback(const clusterisator::Persons::ConstPtr& img_persons){
+	
+	const sensor_msgs::Image img = img_persons->img;
+	
+	recieved_data_pub.publish(img);
+	
+	h = img.height;
+	w = img.width;
+	
+	resolution_factor = img_persons->resolution_factor;
+	
+	CODE_STATIC_OBJECT = img_persons->STATIC_OBJECT;
+	CODE_MOVING_OBJECT = img_persons->MOVING_OBJECT;
+	CODE_MOVING_PERSON = img_persons->MOVING_PERSON;
+	CODE_ROBOT = img_persons->ROBOT;
+	
+	if(img_persons->there_is_a_robot){
+	
+		uint32_t i,j;
+		for(i=0;i<h;i++){
+			for(j=0;j<w;j++){
+				data_type.push_back(img.data[i*3*w+j*3+2]);
+				data_depth.push_back((img.data[i*3*w + j*3 + 1] << 8) + img.data[i*3*w + j*3]);
+			}
+		}
+	
+		collect_robot_data();
+
+		check_security_distance();
+		
 	}
 }
 
